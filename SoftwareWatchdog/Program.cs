e27fb6fd81ed8cb21cc80de38ff445b6575f07d8
Matt Hoffman <lord.matt.hoffman@gmail.com>
2014-04-14 02:38:43 -0700

    Finished an untested version of the Software Watchdog. Added a README.md to the Software Watchdog to describe its purpose/usage/example syntax.

--- SoftwareWatchdog/Program.cs
+++ SoftwareWatchdog/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using MiniJSON;
@@ -10,82 +11,207 @@ namespace arcade_cabinets
 {
     class Program
     {
+        //Used to ensure we only force-focus once. This allows developers to
+        //alt-tab during gameplay without the game popping back up every second.
+        private static bool _completedFocusHack = false;
+
+
         static void Main(string[] args)
         {
             //Ensure they're passing the proper number of arguments.
             if (args.Length != 1)
             {
-#if !DEBUG
                 DebugLog("Invalid number of arguments specified! Usage:");
                 DebugLog("softwarewatchdog.exe <json encoded args here.>");
                 DebugLog("");
                 DebugLog("See included documentation for json string format.");
                 return;
-#endif
             }
 
             //Now let's validate the json.
             var jsonData = Json.Deserialize(args[0]) as Dictionary<string, object>;
             if (jsonData == null)
             {
-
+                DebugLog("Invalid json format specified. Use an online json validator to find out why! Json:");
+                DebugLog(args[0]);
+                return;
             }
 
+            //Now that we know they've given us a valid Json string, lets ensure it has the required output.
+            if (!ValidateRequiredJsonArguments(jsonData))
+            {
+                return;
+            }
 
+            //Okay, we now know we have what we need at bare minimum. Lets launch the game.
+            Process gameProcess = StartGameProcess(jsonData["gameData"] as Dictionary<string, object>);
+            if (gameProcess == null)
+            {
+                DebugLog("Failure launching game process, aborting!");
+            }
+            else
+            {
+                //Now we're going to wait until the game quits.
+                bool bLoop = true;
+                while (bLoop)
+                {
+                    //Ensure the game is in focus (see CheckFocusHack for more details)
+                    CheckFocusHack(gameProcess);
+
+                    bLoop = !gameProcess.HasExited;
+
+                    System.Threading.Thread.Sleep(1000);
+                }
+            } 
+            
+            //Okay we've launched the game, they've played it, they quit. Now we want to launch the launcher again.
+            StartProcess(jsonData["launcherPath"] as string);
         }
 
+        private static Process StartGameProcess(Dictionary<string, object> jsonData)
+        {
+            string gamePath = jsonData["gamePath"] as string;
+            string launchArgs = jsonData["launchParams"] as string ?? "";
+            return StartProcess(gamePath, launchArgs);
+        }
+
+        /// <summary>
+        /// Ensures the json string has been passed in with all of the required information
+        /// to properly run the watchdog.
+        /// </summary>
+        /// <param name="jsonData"></param>
+        /// <returns></returns>
+        private static bool ValidateRequiredJsonArguments(Dictionary<string, object> jsonData)
+        {
+            bool bValid = true;
+
+            //Ensure the launcher we want to re-open exists.
+            string launcherPath = jsonData["launcherPath"] as string;
+            if (launcherPath == null)
+            {
+                DebugLog("Missing launcherPath argument!");
+                bValid = false;
+            }
+            else if (!File.Exists(launcherPath))
+            {
+                DebugLog("Specified launcher doesn't exist! Path:");
+                DebugLog(launcherPath);
+                bValid = false;
+            }
+
+            //Ensure the game we want to open exists.
+            var gameData = jsonData["gameData"] as Dictionary<string, object>;
+            if (gameData == null)
+            {
+                DebugLog("Missing gameData argument!");
+                bValid = false;
+            }
+            else
+            {
+                var gamePath = gameData["gamePath"] as string;
+                if (!File.Exists(gamePath))
+                {
+                    DebugLog("Specified game doesn't exist! Path:");
+                    DebugLog(gamePath);
+                    bValid = false;
+                }
+            }
+
+            return bValid;
+        }
+
+        /// <summary>
+        /// Starts an arbitrary process.
+        /// </summary>
+        /// <param name="exePath">Path to exe (ie: C:\MyGame.exe)</param>
+        /// <param name="launchArgs">Arguments to pass to the executable if required. (ie: -nosound)</param>
+        /// <returns></returns>
+        private static Process StartProcess(string exePath, string launchArgs = "")
+        {
+            //This should work all of the time, but having it fall back to C:\ if it doesn't so the Watchdog doesn't crash.
+            string gameDirectory = Path.GetDirectoryName(exePath) ?? "C:\\";
+
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = exePath;
+            startInfo.WorkingDirectory = gameDirectory;
+            startInfo.Arguments = launchArgs;
+
+            return Process.Start(startInfo);
+        }
+
+        /// <summary>
+        /// Writes out debug information to either the console window (in a Debug application with a window)
+        /// or to the disk alongside the softwarewatchdog.exe. 
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
         private static void DebugLog(string format, params object[] args )
         {
+#if DEBUG
             Console.WriteLine(format, args);
+#else
+            string logFile = "logOutput.txt";
+            File.AppendAllText(logFile, String.Format(format, args) + Environment.NewLine);
+#endif
         }
 
-
-        void Zero()
+        /// <summary>
+        /// During the writing of V2 of this software (this being V3 now...) it was discovered that if you
+        /// spamed on the keyboard while games were loading the game would take over full screen as expected
+        /// but the keyboard focus would not be on the game window and it would break the machine.
+        /// 
+        /// It appears to be a Windows bug that is causing the issue, as it affected all of our games on multiple
+        /// computers (ie: XNA, Flash, Love2D/SDL, GameMaker, etc.).
+        /// 
+        /// During debugging it was found that when a process launches, Windows briefly focuses a program named
+        /// "Process Manager". If no keys are spammed during this time the game opens and takes focus as expected.
+        /// However, if the keyboard is spammed during opening, it appears that this "Process Manager" process never
+        /// loses focus as expected, and thus all keyboard input is sent to this invisible process. 
+        /// 
+        /// To resolve this, we will check which window is focused while the game is running and if it finds that it
+        /// is not the game then it forces the game. This is only done once however, to allow a developer to alt-tab
+        /// via keyboard while the game is running after the game boots.
+        /// </summary>
+        /// <param name="gameProcess">Process of the game which should be in focus.</param>
+        private static void CheckFocusHack(Process gameProcess)
         {
-            bool bLoop = true;
-            Process trackedProcess = null;
-            while (bLoop)
+            if (!_completedFocusHack)
             {
-                Process[] localByName = Process.GetProcessesByName("bako");
-                if (localByName.Length > 0 && trackedProcess == null)
+                //Check the focused window process vs. what's desired.
+                IntPtr focusedWindowHandle = GetForegroundWindow();
+                Process focusedProcess = FindProcess(focusedWindowHandle);
+
+                if (focusedProcess.ProcessName != gameProcess.ProcessName)
                 {
-                    trackedProcess = localByName[0];
+                    SetForegroundWindow(gameProcess.MainWindowHandle);
                 }
-                else if (localByName.Length == 0)
+                else
                 {
-                    Console.WriteLine("Waiting...");
-                    trackedProcess = null;
+                    //If the focused process matches the game process, we should be good.
+                    _completedFocusHack = true;
                 }
-
-                if (trackedProcess != null)
-                {
-                    StringBuilder buffer = new StringBuilder(256);
-                    IntPtr handle = GetForegroundWindow();
-
-                    GetWindowText(handle, buffer, 256);
-
-                    if (buffer.ToString() != trackedProcess.ProcessName)
-                    {
-                        Console.WriteLine("Mismatched focus found. Found: {0} Expected: {1}", buffer.ToString(),
-                            trackedProcess.ProcessName);
-
-                        SetForegroundWindow(trackedProcess.MainWindowHandle);
-                    }
-
-                    Console.WriteLine("Focused Window: " + buffer);
-                }
-
-                System.Threading.Thread.Sleep(250);
             }
         }
 
+        public static Process FindProcess(IntPtr yourHandle)
+        {
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (p.Handle == yourHandle)
+                {
+                    return p;
+                }
+            }
+
+            return null;
+        }
+
+        //DLL Imports for the WinAPI to get/set the focused window.
         [DllImport("user32.dll")]
         static extern IntPtr GetForegroundWindow();
 
         [DllImport("user32.dll")]
         private static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
     }
 }
---
+++ SoftwareWatchdog/README.md
@@ -0,0 +1,27 @@
+Software Watchdog
+===============
+Usage: softwarewatchdog.exe <json arg string>
+
+Description:
+The Software Watchdog will launch the game, minimize all other windows and then wait for the game to close before re-launching the front end.
+
+Json Arguments
+===============
+launcherPath - Full filepath of launcher to open when the game closes. Will be launched right before the Watchdog closes.
+gamePath - Full filepath of game's executable to open when the Watchdog starts.
+launchParams - (Optional) Parameters to pass to the executable if required.
+
+Note: Ensure you use a valid Json string. This can be validated by [Online Json Parsers](http://json.parser.online.fr/). 
+
+
+Example Json String
+===============
+{
+	"launcherPath" : "C:/My Launcher.exe",
+	"gameData" :
+	{
+		"gamePath" : "C:/Game1/My Game.exe",
+		"launchParams" : "-nosound -noborder"
+	}
+}
+
